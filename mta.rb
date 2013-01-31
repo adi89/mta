2d7aa5eb18b2e80f35834c3b6586853433dbb6a6
NickyH <nickyhughes00@gmail.com>
2013-01-30 19:39:20 -0500

    Added arrays

---
+++ mta.rb
@@ -0,0 +1,9 @@
+#***MTA Project***
+
+puts "Select: (6)Line, (N)Line or (L)Line"
+subway_select = gets.chomp
+
+six = ["42", "33", "28", "23", "US", "AS"]
+N = ["TS", "34", "28", "23N", "US", "8"]
+L = ["8", "6", "US", "3", "1"]
+
