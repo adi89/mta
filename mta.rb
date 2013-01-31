33c85f049b3d4968c50e5a35da927471a7c1d458
NickyH <nickyhughes00@gmail.com>
2013-01-30 19:51:05 -0500

    added stops

--- mta.rb
+++ mta.rb
@@ -3,7 +3,19 @@
 puts "Select: (6)Line, (N)Line or (L)Line"
 subway_select = gets.chomp
 
+case subway_select
+  when "6"
+    puts "Starting at: (42) (33) (28) (23) (US) (AS)?"
+    stop_select = gets.chomp
+  when N puts "Starting at: (TS) (34) (28) (23N) (US) (8)?"
+  when L puts "Starting at: (8) (6) (US) (3) (1)?"
+end
+
+
 six = ["42", "33", "28", "23", "US", "AS"]
 N = ["TS", "34", "28", "23N", "US", "8"]
 L = ["8", "6", "US", "3", "1"]
+
+subway = {(six), (N), (L)}
+
 
